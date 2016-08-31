223400ca1fba124cf546d066aa3b2d5907f8bb33
Sean McHenry <seankmchenry@gmail.com>
2016-08-31 00:24:43 -0400

    Moved icons. Fixed mobile nav border. Updated footer margins.

--- content/themes/windowfab/templates/content-page.php
+++ content/themes/windowfab/templates/content-page.php
@@ -11,7 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title center mb3"><?php echo _s_get_the_title(); ?></h1>
+		<h1 class="entry-title center"><?php echo _s_get_the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
